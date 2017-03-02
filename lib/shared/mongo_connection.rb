fb193619e323a8035c92409fa8db35dd6acc0fa1
Peyton Vaughn <pvaughn@6fusion.com>
2017-03-02 11:59:57 -0500

    remove circular dependencies; cleanup mongo initialization - utilize kubernetes

--- lib/shared/mongo_connection.rb
+++ lib/shared/mongo_connection.rb
@@ -1,7 +1,3 @@
-require 'logger' # ruby's logger, to avoid circular dependencies via configuration module
-
-require 'global_configuration'
-
 # This monkey patch allows us to add options to the pipeline
 module Moped
   class Collection
@@ -12,18 +8,16 @@ module Moped
 end
 
 module MongoConnection
-  def initialize_mongo_connection(mongoid_config = GlobalConfiguration::GlobalConfig.instance)
+  def initialize_mongo_connection
     STDOUT.sync = true # disable output buffering; makes it hard to follow docker logs
     load_params = {sessions: {default: {
-        database: mongoid_config[:mongoid_database],
-        hosts: [mongoid_config[:mongoid_hosts]].flatten,
-        options: mongoid_config[:mongoid_options]
-    }}}
+        database: '6fusion_meter',
+        hosts: [ "#{ENV['MONGODB_SERVICE_HOST']}:#{ENV['MONGODB_SERVICE_PORT']}" ] }}}
 
     Mongoid::Config.load_configuration(load_params)
 
-    Mongoid.logger.level = mongoid_config[:mongoid_log_level]
-    Moped.logger.level   = Mongoid.logger.level
+#    Mongoid.logger.level = mongoid_config[:mongoid_log_level]
+#    Moped.logger.level   = Mongoid.logger.level
 
     # !! Mongoid.logger = Logging::MeterLog.instance.logger hmmmm
 
